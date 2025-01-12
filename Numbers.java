64b4798fb328cfc715accaa176b72d4d8aac43f3
samaptidutta <duttasamapti24@gmail.com>
2025-01-12 21:18:01 +0530

    Fourth commit

--- Numbers.java
+++ Numbers.java
@@ -4,5 +4,6 @@ public class Numbers {
     public static void main(String[] args) {
         Prime p = new Prime();
         p.PrimeNumber(19);
+        System.out.println("This is number");
     }
 }
